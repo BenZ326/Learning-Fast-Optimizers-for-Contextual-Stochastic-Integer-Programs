97a00f0dd0b07b1f2760442e91d6d70642e26c80
XiangyiZhang <arccos0_xiangyi@163.com>
2019-01-25 10:38:33 -0500

    reproduce completed, yet notebook is not done

--- assignment 1/part 1/1.1.a.py
+++ assignment 1/part 1/1.1.a.py
@@ -19,6 +19,9 @@ class UCBBandit:
         self.delta = delta
         self.est_dstr = []
         self.total_steps=total_steps
+        self.H1=0
+        for idx in range(1,arms):
+            self.H1 = (1/(self.q_true[0]-self.q_true[idx])**2)+ self.H1
         for i in range(arms):
             self.est_dstr.append([])
 
@@ -30,11 +33,11 @@ class UCBBandit:
         """
         res = []
         for arm, _ in enumerate(self.q_est):
-            #exp_1 = 1 + self.eps ** 0.5
-            #exp_2 = (1 + self.eps) * self.N[arm]
-            #exp = exp_1 * \
-            #     np.sqrt((exp_2 * np.log((np.log(exp_2)+2) / self.delta )) / (2 * self.N[arm]))
-            exp=1.05*0.25/np.log(self.N[arm])
+            exp_1 = 1 + self.eps ** 0.5
+            exp_2 = 1 + self.eps
+            exp_3 = (1 + self.eps) * self.N[arm]
+            exp = exp_1 * \
+                  np.sqrt((exp_2 * np.log((np.log(exp_3 + 2)) / self.delta)) / (self.N[arm]))
             res.append(exp)
         return res
 
@@ -94,7 +97,7 @@ class UCBBandit:
         """
         get accumulated Pr{I_t=i} over trials
         """
-        for t in range(0,self.total_steps):
+        for t in range(self.arms,self.total_steps):
             left_index=max(0,t-self.arms+1)
             right_index=t
             for arm in range(self.arms):
@@ -147,8 +150,8 @@ class AEBandit(UCBBandit):
         exp_2 = 1+self.eps
         exp_3 = (1 + self.eps) * self.N[arm]
         exp = exp_1 * \
-        np.sqrt((exp_2 * np.log((np.log(exp_3+1)) / self.delta )) / ( self.N[arm]))
-        return 2*exp
+        np.sqrt((exp_2 * np.log((np.log(exp_3+2)) / (self.delta/self.N[arm]) )) / ( self.N[arm]))
+        return exp
 
     def update_omega(self,reference_arm):
         removed_arms=set()
@@ -160,7 +163,6 @@ class AEBandit(UCBBandit):
         self.Omega = self.Omega-removed_arms
 
     def learning(self):
-        #while len(self.Omega)>1:
         for i in range(self.total_steps):
             for arm in self.Omega:
                 self.play_arm(int(arm))
@@ -170,7 +172,22 @@ class AEBandit(UCBBandit):
 
 
 
-
+class LUCBBandit(UCBBandit):
+    def select_arm(self):
+        """
+        Select the arm to play for the Bandit
+        """
+        h = np.argmax(self.q_est)
+        C = self._calculate_c()
+        QC = np.sum([self.q_est, C], axis=0)
+        l_ = heapq.nlargest(2, range(self.arms), key=QC.__getitem__)
+        l = l_[0] if h != l_[0] else l_[1]
+        # Select arm
+        # Check stopping criterion
+        if self.q_est[h] - C[h] > self.q_est[l] + C[l]:
+            self.extend_history(h)
+            return h,l, True
+        return h,l,False
 
 def main():
     """
@@ -180,36 +197,45 @@ def main():
     parser.add_argument('--delta', type=float, default=0.1)
     parser.add_argument('--epsilon', type=float, default=0.01)
     parser.add_argument('--trials', type=int, default=5000)
-    parser.add_argument('--steps', type=int, default=7000)
+    parser.add_argument('--steps', type=int, default=2700)
     parser.add_argument('--r_k', type=int, default=1)
     args = parser.parse_args()
 
-    #bandit = UCBBandit(arms=6, delta=args.delta, eps=args.epsilon,total_steps=args.steps)
-    bandit_AE = AEBandit(arms=6, delta=args.delta, eps=args.epsilon,total_steps=args.steps,r_k=args.r_k)
+    bandit = LUCBBandit(arms=6, delta=args.delta, eps=args.epsilon,total_steps=args.steps)
+    #bandit_AE = AEBandit(arms=6, delta=args.delta, eps=args.epsilon,total_steps=args.steps,r_k=args.r_k)
     for trial in range(args.trials):
-        #bandit.initialize()
-        bandit_AE.initialize()
-        bandit_AE.learning()
-        #for step in range(args.steps):
-            #arm, done = bandit.select_arm()
-            #if not done:
-            #    bandit.play_arm(arm)
-            #else:
-            #   print("The best arm is {}".format(arm))
-            #    break
-        #bandit.get_estimate_probability()
-        bandit_AE.get_estimate_probability()
-    bandit_AE.average_est_dstr(args.trials)
-    return bandit_AE
+        bandit.initialize()
+        #bandit_AE.initialize()
+        #bandit_AE.learning()
+        for step in range(args.steps):
+            arm1,arm2, done = bandit.select_arm()
+            if not done:
+                bandit.play_arm(arm1)
+                bandit.play_arm(arm2)
+            else:
+               print("The best arm is {}".format(arm1))
+               break
+        bandit.get_estimate_probability()
+        #bandit_AE.get_estimate_probability()
+    bandit.average_est_dstr(args.trials)
+    return bandit
 
 
 b=main()
-plt.plot(range(len(b.est_dstr[0])),b.est_dstr[0],'.',color='b')
-plt.plot(range(len(b.est_dstr[1])),b.est_dstr[1],'.', color='g')
-plt.plot(range(len(b.est_dstr[2])),b.est_dstr[2],'.',color='r')
-plt.plot(range(len(b.est_dstr[3])),b.est_dstr[3],'.',color='c')
-plt.plot(range(len(b.est_dstr[4])),b.est_dstr[4],'.',color='y')
-plt.plot(range(len(b.est_dstr[5])),b.est_dstr[5],'.',color='k')
+color_set=['b','g','r','c','y','k']
+
+line0,= plt.plot(range(len(b.est_dstr[0]))/b.H1,b.est_dstr[0],'--',color = 'b',markersize=2,label="$\mu_0=1.0$")
+line1,= plt.plot(range(len(b.est_dstr[1]))/b.H1,b.est_dstr[1],'--',color = 'g',markersize=2,label="$\mu_1=0.8$")
+line2,= plt.plot(range(len(b.est_dstr[2]))/b.H1,b.est_dstr[2],'--',color = 'r',markersize=2,label="$\mu_2=0.6$")
+line3,= plt.plot(range(len(b.est_dstr[3]))/b.H1,b.est_dstr[3],'--',color = 'c',markersize=2,label="$\mu_3=0.4$")
+line4,= plt.plot(range(len(b.est_dstr[4]))/b.H1,b.est_dstr[4],'--',color = 'y',markersize=2,label="$\mu_4=0.2$")
+line5,= plt.plot(range(len(b.est_dstr[5]))/b.H1,b.est_dstr[5],'--',color = 'k',markersize=2,label="$\mu_5=0.0$")
+
+
+plt.xlabel("Number of pulls (units of H1)")
+plt.ylabel("$P(I_t=i)$")
+plt.yticks(np.arange(0,1.1,0.1))
+plt.legend(bbox_to_anchor=(0, 0.5, 0.4, 0.5))
 plt.show()
 print(b.history)
 #if __name__ == '__main__':
