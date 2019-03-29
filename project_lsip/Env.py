651df9a154e52a14eb35e4770dc54a632b380738
XiangyiZhang <arccos0_xiangyi@163.com>
2019-03-29 19:35:10 -0400

    finish the env file

---
+++ project_lsip/Env.py
@@ -0,0 +1,54 @@
+# An abstract class of Env
+import numpy as np
+
+class Env:
+    def __init__(self):
+        pass
+    def reset(self):
+        pass
+    def seed(self, seed = None):
+        pass
+    def step(self,action):
+        pass
+
+#Environment for two stage stochastic programming
+
+#######################################################################
+# Authors: Xiangyi Zhang, Rahul Patel                                 #
+#######################################################################
+
+from Model_KS import KS_MIP
+import numpy as np
+
+
+class Env_KS(Env):
+    """
+     instance: a problem instance
+     N_w: the number of scenarios
+    """
+    def __init__(self,instance,N_w):
+        Env.__init__(self)
+        self.instance = instance
+        self.N_w = N_w
+
+
+    """
+    action: a solution popped out by NADE
+    return value: reward
+    """
+    def step(self,action):
+        # get the objective value of first stage
+        f_x = action@self.instance.get_values()
+        f_y = 0         # the expected cost of second stage
+        for scenario in range(self.N_w):
+            model = None
+            while True:
+                weights = self.instance.sample_scenarios()
+                model = KS_MIP(self.instance,action,weights)
+                state = model.solve()
+                if state ==  "optimal":
+                    break
+            f_y += model.query_opt_obj()/self.N_w
+        return f_x + f_y
+
+
